ce77ce69a395b36087ed04da684f7057c1fd2cf7
Ryan <rgwin1@ivytech.edu>
2025-02-16 05:25:56 -0500

    created main.py, created root window, added frame for navigation, and frame for different pages

--- main/main.py
+++ main/main.py
@@ -1,29 +1,47 @@
+import sys
+sys.path.append('C:/Users/rgwin/OneDrive/Desktop/School/Spring2025/SDEV140FP/FamilyTracker')
 import tkinter as tk
 from tkinter import ttk
+from navigation.navbar import NavBar
 
 
 def create_nav_frame(container):
     
-    #define fame
-    frame = tk.Frame(container, bg='blue', height=75)
+    #define frame
+    frame = tk.Frame(container, bg='blue', height=300)
+    #commenting out these for testing
+    # frame.columnconfigure(0, weight=1)
+    # frame.rowconfigure(0, weight=1)
+    return frame
 
-    #grid layout for navbar :: what's happening here?
-    container.columnconfigure(0, weight=1)
+def create_window_frame(container):
+    frame = tk.Frame(container, bg='orange')
 
+    frame.grid(column=0, row=1, sticky='nsew')
+    # frame.columnconfigure(0, weight=1)
+    #frame.rowconfigure(1, weight=1)
 
     return frame
 
 
+# def create_dashboard_window():
+#     #define window
+#     window =tk.Tk()
+#     window.title('Dashboard')
+
+#     window.geometry("500x500")
+
 def create_main_window():
     root = tk.Tk()
     root.title('Family Tracker')
-  
+
     
-    root.columnconfigure(0, weight=4)
-    root.columnconfigure(1, weight=1)
+    #configure main_window rows and columns (this was messed up and prevented me from seeing the window_frame)
+    root.columnconfigure(0, weight=1) #stretches across columns, all available space? I think
 
+    root.rowconfigure(1, weight=1) #starts at row 1 and stretches to fill the remaining space, i think, within root
 
-
+    #set window size
     window_width = 1500
     window_height = 1000
 
@@ -38,8 +56,30 @@ def create_main_window():
     #set the position of the window to the center of the screen
     root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
+    #instantiate window frame
+    window_frame = create_window_frame(root)
+    window_frame.rowconfigure(0, weight=1)
+    window_frame.columnconfigure(0, weight=1)
+    window_frame.grid(column=0, row=1)#navbar is in row 0, windowframe is in row1
+
+
+    #instantiate nav_frame
     nav_frame = create_nav_frame(root)
-    nav_frame.grid(column=0, row=0, sticky='ew', columnspan=2)
+    #look at the configuring in a moment, do I need to add it here if it's added in the navbar.py? 
+    nav_frame.columnconfigure(0, weight=1)
+    nav_frame.rowconfigure(0, weight=1)
+    #add to widget using grid
+    nav_frame.grid(column=0, row=0, sticky='ew')
+
+    #add to widget using pack
+    #nav_frame.pack(anchor="center", pady=10)
+    nav = NavBar(nav_frame, window_frame) #nav_frame gets passed as the parent container to the navbar frame.   See below
+
+
+
+    #instantiate window frame functionality(?)
+
+
 
     root.mainloop()
 
@@ -57,6 +97,32 @@ to do list for main.py:
 4) add frame for planner.py
 5) add frame for contacts/emergency contacts
 6) add frame for menu at the top
+
+
+
+root = Tk() //creates the root object
+
+root.geometry("400x400") // sets the window size in terms of pixels
+
+//create frame 1
+frame1 = Frame(root, bg="black", width=500, height=300) #sets the frame up in size and location (root window)
+frame1.pack() //puts the frame1 in the root object
+
+//create frame 2
+frame2 = Frame(frame1, bg="white", width=100, heigh=100)
+frame2.pack(pady=20, padx=20)
+
+root.mainloop() // do the thing
+
+
+
+
+
+
+
+
+
+
 
 
 """
