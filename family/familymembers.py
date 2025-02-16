b02fda05a10a8209f868130f98531610cdc42cdf
Ryan <rgwin1@ivytech.edu>
2025-02-16 05:22:40 -0500

    created family members page, created family page, added family class for family navigation

---
+++ family/familymembers.py
@@ -0,0 +1,65 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+class FamilyMember():
+    """creates the family member information: firstname, lastname, date of birth, (SSN)? see notes for more examples"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""Notes:
+
+Categories: Each family member will have basic information like name, dob, ssn, etc.
+then, other things that are possible to add can be a categorical add, like, school, work, meds, doctors information,
+etc, think like, drop down box, where the user "adds" each chunk of info.  Add/delete family members for functionality. Family relationship.
+
+profile? well, each family member can have their own login, i guess? we'll worry about that later. 
+
+"""
---
+++ family/familypage.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+class Family():
+    def __init__(self, window_frame):
+        self.frame = tk.Frame(window_frame, bg='red', borderwidth=2, relief='solid')
+        self.frame.grid(column=0, row=0, sticky='nsew')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""Things to consider:
+state
+
+
+
+"""
