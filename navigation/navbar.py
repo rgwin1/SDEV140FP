0a3b373fa6bdd3050f19bc5625aa60534ee7c3e4
Ryan <rgwin1@ivytech.edu>
2025-02-28 22:55:51 -0500

    modified dashboard function

--- navigation/navbar.py
+++ navigation/navbar.py
@@ -12,29 +12,29 @@ class NavBar():
 
     # Initialize the navbar, set up the frame, and store any necessary references
         # self.frame = tk.Frame(container, bg='red', height=25)
-        self.frame = tk.Frame(container, bg='blue')
+        self.navbar_frame = tk.Frame(container, bg='blue')
         self.window_frame = window_frame
  
         # Create the navigation buttons and add them to the navbar
         #dashboard button
-        self.dash_button = tk.Button(self.frame, text="Dashboard", command=self.go_to_dashboard, width=20, height=2)
+        self.dash_button = tk.Button(self.navbar_frame, text="Dashboard", command=self.go_to_dashboard, width=20, height=2)
         self.dash_button.grid(column=0, row=0, sticky="nsew", padx=10, pady=10)
 
         #Family button
-        self.family_button = tk.Button(self.frame, text="Family", command=self.go_to_family, width=20, height=2)
+        self.family_button = tk.Button(self.navbar_frame, text="Family", command=self.go_to_family, width=20, height=2)
         self.family_button.grid(row=0, column=1, sticky='ew', padx=5, pady=5)
 
         #Contact Button
-        self.contact_button = tk.Button(self.frame, text="Contact", command=self.go_to_contact, width=20, height=2)
+        self.contact_button = tk.Button(self.navbar_frame, text="Contact", command=self.go_to_contact, width=20, height=2)
         self.contact_button.grid(row=0, column=2, sticky='ew', padx=5, pady=5)
 
         #Planner Button
-        self.planner_button = tk.Button(self.frame, text="Planner", command=self.go_to_planner, width=20, height=2)
+        self.planner_button = tk.Button(self.navbar_frame, text="Planner", command=self.go_to_planner, width=20, height=2)
         self.planner_button.grid(row=0, column=3, sticky='ew', padx=5, pady=5)
 
         #revisit these now that I have a better understanding how the row/col configuring works
-        self.frame.rowconfigure(0, weight=1)
-        self.frame.grid(row=0, column=0, padx=5, pady=5)
+        self.navbar_frame.rowconfigure(0, weight=1)
+        self.navbar_frame.grid(row=0, column=0, padx=5, pady=5)
         
 
     # Define the functions for handling button clicks and navigation logic
